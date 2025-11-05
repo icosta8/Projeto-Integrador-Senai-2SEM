7c499d98aa942a65e07beb40d987e3a0c90ac9a5
icosta8 <isaque.costa7@senaisp.edu.br>
2025-11-04 21:53:23 -0300

    Atualização 04.11 Pedido

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Login from '../views/Login.vue' 
+import Login from '../views/Login.vue'
+import Pedidos from '../views/Pedidos.vue' 
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/login', name: 'Login', component: Login },
+    { path: '/pedidos', name: 'Pedidos', component: Pedidos}
   ],
 })
 
